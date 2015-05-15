42078b1e095cf29c1edcabe357e8a46ed8f9b30d
user <nobody>
2015-05-15 12:48:43 -0700

    sync

--- gitcommit.el
+++ gitcommit.el
@@ -1,5 +1,13 @@
 (require 'types)
 
+(defmacro with-directory (directory &rest body)
+  `(let ((old default-directory))
+     (unwind-protect
+         (progn
+           (cd ,directory)
+           ,@body)
+       (cd old))))
+
 (defun maybe-git-commit ()
     (shell-command
      (concat
@@ -7,7 +15,8 @@
       (buffer-file-name))))
 
 (defun gitcommit-enhooken ()
-  (add-hook 'after-save-hook 'maybe-git-commit nil 't))
+  (set (make-local-variable 'backup-inhibited) t)
+  (add-hook 'after-save-hook 'maybe-git-commit nil t))
 
 (dolist (type (append edity-types programmy-types))
   (add-hook (type->hook type) 'gitcommit-enhooken))
