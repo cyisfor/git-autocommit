c042423a3d9f4f308c2a3a067086936d22944238
user <nobody>
2017-04-10 19:34:08 +0000

    auto (interpolate.py) 147 867

--- interpolate.py
+++ interpolate.py
@@ -1,18 +1,24 @@
 from sympy import *
-X, x1, y1, x2, y2, x3, y3 = symbols("X x1 y1 x2 y2 x3 y3")
-expr = (y1 * (X - x2) / (x1 - x2) * (X - x3) / (x1 - x3)
-				+
-				y2 * (X - x1) / (x2 - x1) * (X - x3) / (x2 - x3)
-				+
-				y3 * (X - x2) / (x3 - x2) * (X - x1) / (x3 - x1))
+#X, x1, y1, x2, y2, x3, y3 = symbols("X x1 y1 x2 y2 x3 y3")
+X = Symbol("x")
 
-print(latex(expand(expr)))
+x1 = 1
+y1 = 9000
+x2 = 60
+y2 = 60
+x3 = 600
+y3 = 0
 
-def lessX(expr):
-	# every operation on X costs 10
-	c = count_ops(expr,visual=True).subs(X,10000)
-	# every other operation costs 1
-	c = c.replace(Symbol, type(S.One))
-	return c
+def doit(points):
+	expr = 0
+	for j in range(len(points)):
+		l = points[j][1] # yj
+		for m in range(len(points)):
+			if m == j: continue
+			l = l * (x - points[m][0]) / (points[j][0] - points[m][0]);
+		expr = expr + l
+	print(simplify(expr))
 
-#print(simplify(expr, measure=lessX))
+doit([1,9000],
+		 [60,60],
+		 [600,0])
