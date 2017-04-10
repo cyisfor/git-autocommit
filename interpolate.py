31a69f0887598acf6f3104dd52713495a74b083b
user <nobody>
2017-04-10 20:32:06 +0000

    auto (interpolate.py) 77 554

--- interpolate.py
+++ interpolate.py
@@ -1,17 +1,10 @@
 from sympy.polys.polyfuncs import interpolate
+from sympy import simplify, factor
 from sympy.abc import x
 
 def doit(*points):
-	print(interpolate(points,x))
-	return
-	expr = 0
-	for j in range(len(points)):
-		l = points[j][1] # yj
-		for m in range(len(points)):
-			if m == j: continue
-			l = l * (x - points[m][0]) / (points[j][0] - points[m][0]);
-		expr = expr + l
-	print(simplify(expr))
+	points = tuple((a,log(b)) for a,b in points)
+	print(factor(interpolate(list(points),x)))
 
 """
 1 character = 3600s (don't commit)
@@ -22,17 +15,17 @@ def doit(*points):
 		 (600,0)
 """
 
-doit([1,3600],
-		 [60,60],
-		 [600,0])
+doit((1,3600),
+		 (60,60),
+		 (600,0))
 """
 1 word = 3600s
 10 words = 60s
 50 words = now
 """
-doit([1,3600],
-		 [10,60],
-		 [50,0])
+doit((1,3600),
+		 (10,60),
+		 (50,0))
 
 """
 0 lines = 3600s
@@ -40,6 +33,6 @@ doit([1,3600],
 5 lines = 60s
 10 lines = now
 """
-doit([1,600],
-		 [5,60],
-		 [10,0])
+doit((1,600),
+		 (5,60),
+		 (10,0))
