dfd845be42fb336f11d15abdaf842895e290f8d7
user <nobody>
2017-04-10 19:38:32 +0000

    auto (interpolate.py) 332 1772

--- check.c
+++ check.c
@@ -198,52 +198,6 @@ static void commit_later(uv_timer_t* handle) {
 }
 
 static void maybe_commit(CC ctx, char* path, size_t lines, size_t words, size_t characters) {
-	/* lagrange interpolation
-		 given (x1,y1), (x2,y2), (x3,y3)
-		 L(x) = y1 * (X - x2) / (x1 - x2) * (X - x3) / (x1 - x3)
-		        +
-						y2 * (X - x1) / (x2 - x1) * (X - x3) / (x2 - x3)
-					  +
-						y3 * (X - x2) / (x3 - x2) * (X - x1) / (x3 - x1)
-					...group by X as much as possible
-				  y1 * (X - x2) / (x1 - x2) = y1 * X / (x1 - x2) - y1 * x2 / (x1 - x2)
-					y1 * (X - x3) / (x1 - x3) = y1 * X / (x1 - x3) - y1 * x3 / (x1 - x3)
-			    * =====
-					(y1 * X / (x1 - x2)) * (y1 * X / (x1 - x3)) +
-					
-					(y1 * X / (x1 - x2)) * (- y1 * x3 / (x1 - x3)) +
-					(- y1 * x2 / (x1 - x2)) * (y1 * X / (x1 - x3)) +
-					
-					(- y1 * x2 / (x1 - x2)) * (- y1 * x3 / (x1 - x3))
-					=
-					(y1 / (x1 - x2) * y1 / (x1 - x3)) * X^2
-					+ (
-					   y1 / (x1 - x2) * -y1 * x3 / (x1 - x3) +
-						 -y1 * x2 / (x1 - x2) * y1 / (x1 - x3)) * X
-				  +
-					-y1 * x2 / (x1 - x2) * -y1 * x3 / (x1 - x3)
-
-					A = (y1 / (x1 - x2) * y1 / (x1 - x3))
-					B = (y1 * y1 / (x1 - x2) / (x1 - x3) * (x3 + x2)
-					C = y1 * x2 / (x1 - x2) * y1 * x3 / (x1 - x3)
-	*/
-	/* 1 character = 9000s (don't commit)
-		 60 characters = 60s to commit
-		 600 characters means commit now.
-		 (1,9000)
-		 (60,60)
-		 (600,0)
-	*/
-#define A (y1 / (x1 - x2) * y1 / (x1 - x3))
-#define B (y1 * y1 / (x1 - x2) / (x1 - x3) * (x3 + x2))
-#define C (y1 * x2 / (x1 - x2) * y1 * x3 / (x1 - x3))
-
-#define x1 1
-	
-	#define
-	// m = (60 - 0) / (60 - 600) = - 60 / 540
-	// d = m * c + b, 0 = m * 60 + b, b = -m * 60 = 3600 / 540 = 60 / 9
-	// d = 60 * (1/9 - c / 540)
 	double delay1 = 60 * (1.0 / 9 - characters / 540.0);
 	// 10 words = 1min to commit, 50 words = commit now
 	// m = (60 - 0) / (10 - 50) = -60 / 40 = -3/2
--- interpolate.py
+++ interpolate.py
@@ -1,15 +1,17 @@
 from sympy import *
 #X, x1, y1, x2, y2, x3, y3 = symbols("X x1 y1 x2 y2 x3 y3")
-X = Symbol("x")
+x = Symbol("x")
 
-x1 = 1
-y1 = 9000
-x2 = 60
-y2 = 60
-x3 = 600
-y3 = 0
-
-def doit(points):
+"""	lagrange interpolation
+		 given (x1,y1), (x2,y2), (x3,y3)
+		 L(x) = y1 * (X - x2) / (x1 - x2) * (X - x3) / (x1 - x3)
+		        +
+						y2 * (X - x1) / (x2 - x1) * (X - x3) / (x2 - x3)
+					  +
+						y3 * (X - x2) / (x3 - x2) * (X - x1) / (x3 - x1)
+					...group by X as much as possible
+"""
+def doit(*points):
 	expr = 0
 	for j in range(len(points)):
 		l = points[j][1] # yj
@@ -19,6 +21,34 @@ def doit(points):
 		expr = expr + l
 	print(simplify(expr))
 
-doit([1,9000],
+"""
+1 character = 3600s (don't commit)
+		 60 characters = 60s to commit
+		 600 characters means commit now.
+		 (1,9000)
+		 (60,60)
+		 (600,0)
+"""
+
+doit([1,3600],
 		 [60,60],
 		 [600,0])
+"""
+1 word = 3600s
+10 words = 60s
+50 words = now
+"""
+doit([1,3600],
+		 [10,60],
+		 [50,0])
+
+"""
+0 lines = 3600s
+1 line = 600s
+5 lines = 60s
+10 lines = now
+"""
+doit([0,3600],
+		 [1,600],
+		 [5,60],
+		 [10,0])
