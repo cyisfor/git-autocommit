da1c4ec9e1274cb3eb0bf6801154b986643f0523
user <nobody>
2017-04-11 16:47:12 +0000

    auto (/home/code/git/autocommit/testgit.c) 5 59 611

--- repo.c
+++ repo.c
@@ -2,4 +2,18 @@
 
 git_repository* repo = NULL;
 void repo_init(void) {
-	git_repository_open(&repo,
+	int cwd = -1;
+	for(;;) {
+		if(0 == git_repository_open(&repo,".")) break;
+		if(cwd == -1) {
+			cwd = open(".",O_PATH|O_DIRECTORY);
+			assert(cwd >= 0);
+		}
+		// go up until we find one
+		assert(0 == chdir(".."));
+	}
+	if(cwd != -1) {
+		fchdir(cwd);
+		close(cwd);
+	}
+}
--- testgit.c
+++ testgit.c
@@ -1,23 +1,7 @@
-#include <git2/repository.h>
-#include <git2/errors.h>
-#include <assert.h>
-#include <unistd.h> // getcwd
-
+#include "repo.h"
 
 int main(int argc, char *argv[])
 {
-	const char wd[PATH_MAX];
-	getcwd(wd,PATH_MAX);
-
-  git_libgit2_init();
-
-	git_repository* repo = NULL;
-	git_error_code code = git_repository_open(&repo,wd);
-	if(code != 0) {
-		const git_error* last = giterr_last();
-		assert(last);
-		puts(last->message);
-		return 23;
-	}
+	repo_init();
 	return 0;
 }
