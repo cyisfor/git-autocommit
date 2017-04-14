a81c0f60a16b26ff33fe4e6a5e3932872650237d
autocommit <autocommit>
2017-04-14 01:33:03 +0000

    auto (README.md) 38 309 2422

---
+++ gitcommit.el
@@ -0,0 +1,31 @@
+(require 'types)
+
+(defmacro with-directory (directory &rest body)
+  `(let ((old default-directory))
+     (unwind-protect
+         (progn
+           (cd ,directory)
+           ,@body)
+       (cd old))))
+
+(defun maybe-git-commit ()
+  (interactive)
+  (let* ((buffer (get-buffer-create "*Git Commit Thingy*"))
+		 (process (get-buffer-process buffer)))
+	(setenv "file" (buffer-file-name))
+	(start-process "Git Commit Thingy"
+				   buffer
+				   (expand-file-name "~/code/git/autocommit/client"))
+	(setenv "file")))
+
+(defun gitcommit-enhooken ()
+  (set (make-local-variable 'backup-inhibited) t)
+  (add-hook 'after-save-hook 'maybe-git-commit nil t))
+
+(dolist (type (append edity-types))
+  (add-hook (type->hook type) 'gitcommit-enhooken))
+
+(add-hook 'prog-mode-hook 'gitcommit-enhooken)
+
+
+(provide 'gitcommit)
