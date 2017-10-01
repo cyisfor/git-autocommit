ba1d01f9d1648060f256b4db1bcbf6c1ee8db1c6
autocommit <autocommit>
2017-10-01 04:40:08 +0000

    auto 0 92 979

--- checkpid.c
+++ checkpid.c
@@ -13,9 +13,16 @@ struct after {
 	struct after* next;
 };
 
-struct after* afters;
+struct after* afters = NULL;
 
-static void onchld(int sig) {
+static void alloc_cb(uv_handle_t* handle, size_t size, uv_buf_t* ret) {
+	ret->len = (((size / sizeof(struct signalfd_siginfo))+1)*sizeof(struct signalfd_siginfo));
+	ret->base = realloc(ret->base,ret->len);
+}
+
+static void get_reply(uv_stream_t* stream, ssize_t nread, const uv_buf_t* buf) {
+	ensure_ge(nread,0);
+	/* ignore the data read. ssi_status only gets the first process, not all of them */
 	int pid;
 	int status;
 	void erra(char* fmt, ...) {
@@ -26,7 +33,11 @@ static void onchld(int sig) {
 		va_end(arg);
 		fputc('\n',stderr);
 	}
-	while(-1 != (pid = waitpid(-1, &status, WNOHANG))) {
+	for(;;) {
+		pid = waitpid(-1, &status, WNOHANG);
+		if(pid == 0) break;
+		ensure_gt(pid,0);
+		
 		if(WIFSIGNALED(status)) {
 			erra("died with signal %d",WTERMSIG(status));
 		} else if(WIFEXITED(status)) {
@@ -37,7 +48,6 @@ static void onchld(int sig) {
 		} else {
 			erra("whu? %d",status);
 		}
-
 		struct after* cur = afters;
 		while(cur) {
 			if(cur->pid == pid) {
@@ -54,7 +64,7 @@ static void onchld(int sig) {
 			cur = cur->next;
 		}
 	}
-}
+}				
 
 void checkpid_after(int pid, uv_async_t* async) {
 	struct after* a = malloc(sizeof(struct after));
@@ -64,31 +74,29 @@ void checkpid_after(int pid, uv_async_t* async) {
 	a->prev = NULL;
 	afters->prev = a;
 	afters = a;
-	if(pid == waitpid(pid,NULL,WNOHANG)) {
-		// then the sigchld handler gets invoked here
-		// but uv_async_send coalesces, so it's still only called once.
-		afters = afters->next;
-		afters->prev = NULL;
-		uv_async_send(a->async);
-		free(a);
+	
+}
+
+static sigset_t blocked;
+static uv_pipe_t sfd;
+
+// eh... only useful if you're about to exec.
+int checkpid_fork() {
+	int pid = fork();
+	if(pid == 0) {
+		sigprocmask(SIG_UNBLOCK,&blocked, NULL);
 	}
+	return pid;
 }
 
 void checkpid_init(void) {
-	struct sigaction sa = {
-		.sa_handler = onchld,
-		.sa_flags = SA_RESTART | SA_NOCLDSTOP
-	};
-	assert0(sigaction(SIGCHLD,&sa,NULL));
-}
+	sigemptyset(&blocked);
+	sigaddset(&blocked, SIGCHLD);
+	ensure0(sigprocmask(SIG_BLOCK, &blocked, NULL));
 
-void checkpid(int pid, const char* fmt, ...) {
-	printf("launch %d ",pid);
-	va_list arg;
-	va_start(arg, fmt);
-	vfprintf(stdout, fmt, arg);
-	va_end(arg);
-	fputc('\n',stdout);
-	// then just let the handler catch it...
-	// we don't want to allocate memory for messages about every pending process?
+	int s = signalfd(-1, &blocked, 0);
+	ensure_ge(s,0);
+	ensure0(uv_pipe_init(uv_default_loop(), &sfd, 0));
+	ensure0(uv_pipe_open(&sfd, s));
+	uv_read_start((uv_stream_t*)&sfd, alloc_cb, get_reply);
 }
