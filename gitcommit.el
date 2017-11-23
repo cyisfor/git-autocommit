7763c93dbee9df7bd31c9e5b0b689707259c9310
autocommit <autocommit>
2017-11-23 02:56:50 +0000

    auto 0 54 652

--- gitcommit.el
+++ gitcommit.el
@@ -1,5 +1,7 @@
 (require 'types)
 
+(defvar-local do-git-commit t)
+
 (defmacro with-directory (directory &rest body)
   `(let ((old default-directory))
      (unwind-protect
@@ -10,14 +12,14 @@
 
 (defun maybe-git-commit ()
   (interactive)
-  (let* ((buffer (get-buffer-create "*Git Commit Thingy*"))
-		 (process (get-buffer-process buffer)))
-	(message "Process %s %s" process (buffer-file-name))
-	(setenv "file" (buffer-file-name))
-	(start-process "Git Commit Thingy"
-				   buffer
-				   (expand-file-name "~/code/git/autocommit/client"))
-	(setenv "file")))
+	(when do-git-commit
+		(let* ((buffer ((get SYMBOL PROPNAME)-buffer-create "*Git Commit Thingy*"))
+					 (process (get-buffer-process buffer)))
+			(setenv "file" (buffer-file-name))
+			(start-process "Git Commit Thingy"
+										 buffer
+										 (expand-file-name "~/code/git/autocommit/client"))
+			(setenv "file"))))
 
 (defun gitcommit-enhooken ()
   (set (make-local-variable 'backup-inhibited) t)
